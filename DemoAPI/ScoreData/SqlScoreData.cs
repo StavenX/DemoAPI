4f2a87a615941dcd6a91330de36d5f2022430e15
Staven <edvin_myhren@hotmail.com>
2021-12-07 21:44:17 +0100

    Scores work in database

---
+++ DemoAPI/ScoreData/SqlScoreData.cs
@@ -0,0 +1,51 @@
+﻿using DemoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAPI.ScoreData
+{
+    public class SqlScoreData : IScoreData
+    {
+        private PlayerContext _playerContext;
+
+        public SqlScoreData(PlayerContext playerContext)
+        {
+            _playerContext = playerContext;
+        }
+
+        public Score AddScore(Guid playerId, int scoreValue, DateTime startedPlaying, DateTime endedPlaying)
+        {
+            var newScore = new Score();
+
+            newScore.Id = Guid.NewGuid();
+            newScore.PlayerId = playerId;
+            newScore.ScoreValue = scoreValue;
+            newScore.StartedPlaying = startedPlaying;
+            newScore.EndedPlaying = endedPlaying;
+
+            _playerContext.Scores.Add(newScore);
+            _playerContext.SaveChanges();
+
+            return newScore;
+        }
+
+        public void DeleteScore(Score score)
+        {
+            _playerContext.Scores.Remove(score);
+            _playerContext.SaveChanges();
+        }
+
+        public Score GetScore(Guid id)
+        {
+            var score = _playerContext.Scores.Find(id);
+            return score; 
+        }
+
+        public List<Score> GetScores()
+        {
+            return _playerContext.Scores.ToList();
+        }
+    }
+}
--- DemoAPI/Startup.cs
+++ DemoAPI/Startup.cs
@@ -37,6 +37,7 @@ namespace DemoAPI
             //services.AddSingleton<IScoreData, MockScoreData>();
 
             services.AddScoped<IPlayerData, SqlPlayerData>();
+            services.AddScoped<IScoreData, SqlScoreData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
