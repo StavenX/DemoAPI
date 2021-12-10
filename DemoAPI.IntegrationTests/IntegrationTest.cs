24986ad727d6964e82416e6602e2edacc12a8563
Staven <edvin_myhren@hotmail.com>
2021-12-10 01:07:11 +0100

    Add test that currently doesn't work

---
+++ DemoAPI.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace DemoAPI.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            TestClient = appFactory.CreateClient();
+        }
+    }
+}
