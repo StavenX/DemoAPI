cc1127e7ae41e461df6659fe57cfe48ba6b9da8d
Staven <edvin_myhren@hotmail.com>
2021-12-12 02:48:55 +0100

    Document LINQ-extension method

--- DemoAPI/Extensions/LinqExtensions.cs
+++ DemoAPI/Extensions/LinqExtensions.cs
@@ -5,6 +5,14 @@ namespace DemoAPI.Extensions
 {
     public static class LinqExtensions
     {
+        /// <summary>
+        /// Retrieves a distinct collection where uniqueness is based on the given key.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns>A disctinct collection.</returns>
         public static IEnumerable<TSource> DistinctBy<TSource, TKey>
             (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
         {
